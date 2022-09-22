e4c72453ea8c455831fa2153d88d8c395465271a
Wen71 <wen.sun@mnsu.edu>
2022-09-22 09:47:31 -0500

    Added business id and Url id to csv file

--- checker.py
+++ checker.py
@@ -4,10 +4,13 @@ import csv
 import time
 
 SLEEP = 0 # Time in seconds the script should wait between requests
+bid_list = []
+urlId_list = []
 url_list = []
 url_statuscodes = []
-url_statuscodes.append(["url","status_code"]) # set the file header for output
+url_statuscodes.append(["bid","url_id","url","status_code"]) # set the file header for output
 
+base_dir = 'C:/Users/Wen Sun/PycharmProjects/BBB/'
 
 def getStatuscode(url):
     try:
@@ -20,20 +23,25 @@ def getStatuscode(url):
 
 # Url checks from file Input
 # use one url per line that should be checked
-with open('urls.csv', newline='') as f:
+with open(base_dir+'mn_bbb_urls_1 40 rows.csv', newline='') as f:
+
     reader = csv.reader(f)
     for row in reader:
-        url_list.append(row[0])
-
+        bid_list.append(row[1])
+        urlId_list.append(row[2])
+        url_list.append(row[3])
+    print(url_list)
 
 # Loop over full list
-for url in url_list:
-    print(url)
-    check = [url,getStatuscode(url)]
+
+for i in range(1, len(url_list)):
+    #print(url)
+    check = [bid_list[i],urlId_list[i],url_list[i],getStatuscode(url_list[i])]
     time.sleep(SLEEP)
     url_statuscodes.append(check)
+print(url_statuscodes)
 
 # Save file
-with open("urls_withStatusCode.csv", "w", newline="") as f:
+with open(base_dir+"urls_withStatusCode.csv", "w", newline="") as f:
     writer = csv.writer(f)
     writer.writerows(url_statuscodes)
