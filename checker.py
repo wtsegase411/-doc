96ee92b9ad7c7bad1ac002826b52c7202e896796
Medhanit Asrat <medhanitabekele@gmail.com>
2022-09-24 15:13:47 -0500

    tranche

--- checker.py
+++ checker.py
@@ -3,7 +3,7 @@ import requests
 import csv
 import time
 
-SLEEP = 0 # Time in seconds the script should wait between requests
+SLEEP = 0  # Time in seconds the script should wait between requests
 url_list = []
 url_statuscodes = []
 url_statuscodes.append(["url","status_code"]) # set the file header for output
@@ -17,21 +17,22 @@ def getStatuscode(url):
     except:
         return -1
 
-
+base_dir = "C:/Users/Medhanit Asrat/PycharmProjects/BBB/"
 # Url checks from file Input
 # use one url per line that should be checked
-with open('urls.csv', newline='') as f:
+with open(base_dir+'mn_bbb_urls.csv', newline='') as f:
     reader = csv.reader(f)
     for row in reader:
-        url_list.append(row[0])
+        url_list.append(row[3])
 
 
 # Loop over full list
 for url in url_list:
-    print(url)
-    check = [url,getStatuscode(url)]
-    time.sleep(SLEEP)
-    url_statuscodes.append(check)
+    #print(url)
+    for i in range(len(url_list)):
+        check = [url[i],getStatuscode(url)]
+        time.sleep(SLEEP)
+        url_statuscodes.append(check)
 
 # Save file
 with open("urls_withStatusCode.csv", "w", newline="") as f:
--- url.py
+++ url.py
@@ -1,4 +1,20 @@
+import csv
+
 import pandas
-base_dir = 'C:/Users/Wen Sun/PycharmProjects/BBB/'
-csvFile = pandas.read_csv(base_dir+'mn_bbb_urls.csv',nrows=10)['URL']
-print(csvFile)
+
+base_dir = 'C:/Users/Medhanit Asrat/PycharmProjects/BBB/'
+csvFile = pandas.read_csv(base_dir + 'mn_bbb_urls.csv', nrows=10)['URL']
+
+
+trunche_url = []
+
+#print(trunche_url)
+with open("url_trunche.csv", "w", newline="") as f:
+    writer = csv.writer(f)
+    trunche_url.append(csvFile)
+    writer.writerow(trunche_url)
+
+    for line in trunche_url:
+        writer = csv.writer(f, delimiter=',')
+        writer.writerow(line)
+#print(csvFile)
