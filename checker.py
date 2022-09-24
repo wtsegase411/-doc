872230f3894c3f4c5d1845f9de321926d856a90e
Wengel <wengelalemayehu19@gmail.com>
2022-09-24 13:46:38 -0500

    Checker code

--- checker.py
+++ checker.py
@@ -5,17 +5,17 @@ import requests
 import csv
 import time
 
-SLEEP = 0 # Time in seconds the script should wait between requests
+SLEEP = 0  # Time in seconds the script should wait between requests
 bid_list = []
 urlId_list = []
 url_list = []
 url_statuscodes = []
-badSyntax = []
-badSyntax.append(["bid","url_id","url"]) # set the file header for output
+badSyntax = [["bid", "url_id", "url"]]
 
-url_statuscodes.append(["bid","url_id","url","status_code"]) # set the file header for output
+url_statuscodes.append(["bid", "url_id", "url", "status_code"])  # set the file header for output
 
 base_dir = 'C:/Users/Wenge/Desktop/BBB/'
+
 
 def checkSyntax(url):
     url_pattern = "^https?:\\/\\/(?:www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&\\/=]*)$"
@@ -38,8 +38,7 @@ def getStatuscode(url):
 # Url checks from file Input
 # use one url per line that should be checked
 
-with open(base_dir+'mn_bbb_urls_140rows.csv', newline='') as f:
-
+with open(base_dir + 'mn_bbb_urls_140rows.csv', newline='') as f:
     reader = csv.reader(f)
     for row in reader:
         url_list.append(row[3])
@@ -56,7 +55,7 @@ for i in range(1, len(url_list)):
         time.sleep(SLEEP)
         url_statuscodes.append(check)
     else:
-        bad= [bid_list[i], urlId_list[i], url_list[i]]
+        bad = [bid_list[i], urlId_list[i], url_list[i]]
 
         badSyntax.append(bad)
         with open("badSyntaxWrite.csv", "w", newline="") as f:
@@ -64,6 +63,6 @@ for i in range(1, len(url_list)):
             writer.writerows(badSyntax)
 
 # Save file
-with open(base_dir+"urls_withStatusCode.csv", "w", newline="") as f:
+with open(base_dir + "urls_withStatusCode.csv", "w", newline="") as f:
     writer = csv.writer(f)
     writer.writerows(url_statuscodes)
